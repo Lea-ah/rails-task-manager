3bc83b3ea7e39c1ced9de565b6c005bce8945779
lea moesch <leamoesch@gmail.com>
2022-05-24 16:57:45 +1000

    RAILS task manager DONE

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # verb 'path', to: 'controller#action', as: 'prefix'
+  # See all tasks:
+  get 'tasks', to: 'tasks#index'
+
+  # Create a task
+  # 1. GET the TASK creation form
+  get 'tasks/new', to: 'tasks#new', as: 'task_new'
+  # 2. POST the parameters to create a new task
+  post 'tasks', to: 'tasks#create', as: 'task_create'
+
+  # Update:
+  get 'tasks/:id/edit', to: 'tasks#edit'
+
+  # See a specific task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
